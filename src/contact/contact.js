4be236717005e2c6c05117e097031f6ebc90fea9
Sukhdeep Singh <sukh.butalia@gmail.com>
2022-04-21 17:08:32 +0530

    Add form in contact component and style the same

--- src/contact/contact.js
+++ src/contact/contact.js
@@ -5,7 +5,36 @@ const contact = () => {
   contact.classList.add('contact');
   contact.setAttribute('id', 'contact');
 
-  contact.textContent = 'CONTACT';
+  const heading = document.createElement('h2');
+  heading.textContent = 'Contact Us';
+  contact.appendChild(heading);
+
+  const form = document.createElement('form');
+
+  const emailField = document.createElement('input');
+  emailField.setAttribute('type', 'email');
+  emailField.setAttribute('placeholder', 'Enter your email...');
+  emailField.setAttribute('required', true);
+  form.appendChild(emailField);
+
+  const msgField = document.createElement('textarea');
+  msgField.setAttribute('placeholder', 'Enter your message');
+  msgField.setAttribute('rows', 7);
+  msgField.setAttribute('required', true);
+  form.appendChild(msgField);
+
+  const btn = document.createElement('button');
+  btn.setAttribute('type', 'submit');
+  btn.textContent = 'Send';
+  form.appendChild(btn);
+
+  contact.appendChild(form);
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    alert('Your message has been sent. We will reach you shortly');
+    form.reset();
+  });
 
   return contact;
 };
