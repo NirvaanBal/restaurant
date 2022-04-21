e7a0c8c499b94756873bbbf40b88f84b0362f625
Sukhdeep Singh <sukh.butalia@gmail.com>
2022-04-21 12:49:46 +0530

    Add welcome component

---
+++ src/home/welcome.js
@@ -0,0 +1,19 @@
+import './home.css';
+
+const home = () => {
+  const rootDiv = document.createElement('div');
+  rootDiv.classList.add('home');
+
+  const heading = document.createElement('h1');
+  heading.textContent = 'The Lemon Club';
+  rootDiv.appendChild(heading);
+
+  const about = document.createElement('p');
+  about.textContent =
+    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt nemo velit similique earum nulla atque unde neque sequi dolorem facilis voluptate architecto, rem vitae dolore sunt libero molestias. Et, perspiciatis?';
+  rootDiv.appendChild(about);
+
+  return rootDiv;
+};
+
+export default home;
