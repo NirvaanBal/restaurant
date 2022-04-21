5f13ec964de1edd30edecddc677818f235aea39f
Sukhdeep Singh <sukh.butalia@gmail.com>
2022-04-21 16:54:26 +0530

    Add styles to menu component

--- src/menu/menu.js
+++ src/menu/menu.js
@@ -1,5 +1,8 @@
 import './menu.css';
 import drink1 from '../assets/drink1.jpg';
+import drink2 from '../assets/drink2.jpg';
+import drink3 from '../assets/drink3.jpg';
+import drink4 from '../assets/drink4.jpg';
 
 const menu = () => {
   const menu = document.createElement('div');
@@ -11,13 +14,54 @@ const menu = () => {
 
   menu.appendChild(heading);
 
+  const drinksArray = [
+    {
+      drink: drink1,
+      title: 'The Green Slush',
+      about:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, laboriosam!',
+    },
+    {
+      drink: drink2,
+      title: 'Pomegranate Reign',
+      about: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    },
+    {
+      drink: drink3,
+      title: 'The Royal Mango',
+      about:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, laboriosam!',
+    },
+    {
+      drink: drink4,
+      title: 'Virgin Mojito',
+      about:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, laboriosam!',
+    },
+  ];
+
   const drinks = document.createElement('ul');
-  for (let i = 1; i <= 1; i++) {
-    const drink = document.createElement('li');
-    const image = new Image();
-    image.src = drink1;
-    drink.appendChild(image);
-    drinks.appendChild(drink);
+  let counter = 1;
+  for (let drink of drinksArray) {
+    const drinkItem = document.createElement('li');
+    if (counter % 2 === 0) drinkItem.classList.add('flip');
+    const drinkImg = new Image();
+    drinkImg.src = drink.drink;
+    drinkItem.appendChild(drinkImg);
+
+    const aboutDiv = document.createElement('div');
+
+    const drinkTitle = document.createElement('h4');
+    drinkTitle.textContent = drink.title;
+    aboutDiv.appendChild(drinkTitle);
+
+    const drinkCaption = document.createElement('p');
+    drinkCaption.textContent = drink.about;
+    aboutDiv.appendChild(drinkCaption);
+
+    drinkItem.appendChild(aboutDiv);
+    drinks.appendChild(drinkItem);
+    counter++;
   }
 
   menu.appendChild(drinks);
